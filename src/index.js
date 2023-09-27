b293e8f91f97ff57fcbc92f58a726d3de5ba1d23
Rainer Simon <rainer@rainersimon.io>
2023-09-27 07:23:19 +0200

    Minor tweaks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import { build } from 'biiif';
 
-build('images/merian', 'http://localhost/images/merian');
+const IMAGES = [
+  // 'caminha2',
+  // 'castelo_bom1',
+  'castelo_branco1',
+  'chaves2',
+  'lapela1',
+  'melgaco2',
+  'mirando_do_douro2',
+  'mirando_do_douro2_Altered',
+  'mogadouro1',
+  'monforte_do_rio_livre1',
+  'monforte2',
+  'moura2',
+  'nisa1',
+  'ouguela1',
+  'segura1',
+  'vinhais1'
+]
+
+for (const image of IMAGES) {
+  build(`images/book_of_fortresses/${image}`, `https://iiif.rainersimon.io/book-of-fortresses/${image}`);
+}
